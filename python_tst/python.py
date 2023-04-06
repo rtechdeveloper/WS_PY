8f7a09fdf944b2feb7d41494581b9d814e641fd3
Rogério <ramorim83@gmail.com>
2023-04-06 07:35:50 -0300

    Ajuste_rpo

---
+++ python_tst/python.py
@@ -0,0 +1,57 @@
+import math
+import os
+
+#===========================================================
+#--- Refatorando - Aula04 Numeros --
+#===========================================================
+
+print("teste GIT HUB, novamente")
+
+''''
+#--- Refatorando - Aula02 Variaveis ---
+nome = "Rogerio"
+sobrenome = "Amorim" 
+idade = 39 
+altura = 1.70 
+carrer = "Developer"
+
+
+#fstring serve para colocar uma variavel dentro do print utilizando o 'f' e usando {nome da variavel}
+print(f"Nome: {nome} {sobrenome} tem {idade} anos")
+print(f"Carrer: {carrer}")
+
+#===========================================================
+#--- Refatorando - Aula03 Numeros --
+#===========================================================
+num1 = 1
+num2 = 2
+
+#type mostra qual o tipo de variavel
+#print(type(num1))
+
+#alterando o tipo de variavel ex: de int para float
+#num3 = float(num1)
+#print(num3)
+#print(type(num3))
+
+#-----------------      Contas Aritimeticas-
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 ** num2)
+print(num1 // num2) 
+
+print(abs(-15)) #valor absoluto
+print(pow(3,3)) #igual exponenciação
+print(max(1,5)) #retorna o valor maior
+print(min(1,3,5)) #retrona o valor menor
+print(round(8.3)) #arredondamento 8.3 = 8 ou 8.6 = 9
+
+#------------ usando a biblioteca math
+print(math.floor(8.9999)) #floor sempre arredondara para baixo
+print(math.ceil(8.0001)) #ceil sempre arredondara para cima
+print(math.sqrt(9)) #raiz quadrada
+'''
+
